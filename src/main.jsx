9194c47cb9c649f97ac6a410a6dd2c2108db7b59
Qasim-asb <alishah.qasim4@gmail.com>
2026-08-13 14:25:27 +0500

    Set up API integration tools

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import './index.css'
 import App from './App.jsx'
 import { WatchlistProvider } from './context/WatchlistContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <WatchlistProvider>
-        <App />
-      </WatchlistProvider>
+      <QueryClientProvider client={queryClient}>
+        <WatchlistProvider>
+          <App />
+        </WatchlistProvider>
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
