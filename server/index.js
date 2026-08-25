52f8a5060f82945b216e607cc21a1edd3726f51e
Qasim-asb <alishah.qasim4@gmail.com>
2026-08-25 15:19:19 +0500

    refactor: refine express app configuration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import process from 'node:process'
 import movieRoutes from './src/routes/movieRoutes.js'
 import authRoutes from './src/routes/authRoutes.js'
 import watchlistRoutes from './src/routes/watchlistRoutes.js'
@@ -16,7 +15,7 @@ app.use(
   })
 )
 
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({
